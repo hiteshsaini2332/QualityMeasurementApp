f7a3d2d8441e23093fb60c19ac7bc07c3a5eb66d
hiteshsaini2332 <hiteshsaini9466@gmail.com>
2026-03-17 10:49:26 +0530

    Copy Created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.uc1.Feet;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -8,8 +7,6 @@ public class Main {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Feet f1=new Feet(1.0);
-        Feet f2=new Feet(2.0);
-        System.out.println(f1.equals(f2));
+
     }
 }
