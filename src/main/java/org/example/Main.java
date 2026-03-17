9a2aac58cd0101de4d867748468e4faf107bf55b
hiteshsaini2332 <hiteshsaini9466@gmail.com>
2026-03-17 12:10:55 +0530

    Changed Content

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,6 @@ public class Main {
     static void main() {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Feet f1=new Feet(1.0);
-        Feet f2=new Feet(2.0);
-        System.out.println(f1.equals(f2));
+
     }
 }
